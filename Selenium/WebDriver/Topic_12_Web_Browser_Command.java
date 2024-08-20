33636a977e26df6d3936bf3af22a972c88fb9a5f
minhtien1650 <minh.tien.1650@gmail.com>
2024-08-20 16:28:57 +0700

    update

--- Selenium/WebDriver/Topic_12_Web_Browser_Command.java
+++ Selenium/WebDriver/Topic_12_Web_Browser_Command.java
@@ -7,6 +7,8 @@ import java.util.List;
 import java.util.Set;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
