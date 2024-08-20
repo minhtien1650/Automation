8a521f879bd4e06f3094508d522ee89eed69c334
minhtien1650 <minh.tien.1650@gmail.com>
2024-08-21 00:26:43 +0700

    update

--- Selenium/WebDriver/Topic_13_Web_Element_Command.java
+++ Selenium/WebDriver/Topic_13_Web_Element_Command.java
@@ -1,11 +1,17 @@
 package WebDriver;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -13,33 +19,202 @@ import org.testng.annotations.Test;
 public class Topic_13_Web_Element_Command {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
+	TakesScreenshot scrShot;
 
 	@BeforeClass
 	public void BeforeCLass() {
 		driver = new EdgeDriver();
-		
+
+		scrShot = ((TakesScreenshot) driver);
+
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 	}
 
-	@Test
+	// @Test
 	public void ElementCommand() {
 		// WebBrowser command/ method/ API
 		// driver instance/ variable
-		
+
 		// WebBrowser command/ method/ API
 		// 1
 		driver.findElement(By.name("Login")).click();
-		
+
 		// 2
 		WebElement emailTextBox = driver.findElement(By.id("email"));
 		emailTextBox.clear();
 		emailTextBox.sendKeys("abc@gmail.com");
 		emailTextBox.isDisplayed();
-		
+
 		WebElement element = driver.findElement(By.id(""));
+
+		// Xóa dữ liệu trong editable field (textbox/ textarea/ dropdown)
+		element.clear(); // **
+
+		// Nhập dữ liệu vào editable field (textbox/ textarea/ dropdown)
+		element.sendKeys(""); // **
+		element.sendKeys(Keys.ENTER);
+
+		// Click vào button/ radio/ checkbox/ image/...
+		element.click(); // **
+
+		// Trả về dữ liệu nằm trong attribute của element
+		element.getAttribute("placeholder"); // **
+		// Email address or phone number
+
+		// Lấy thuộc tính của element: font size/size/color/font style/...
+		element.getCssValue("background-color"); // *
+
+		// GUI
+		element.getLocation();
+		element.getRect();
+		element.getSize();
+
+		// Take screenshot -> Attach to HTML report
+		// Java Generic
+		element.getScreenshotAs(OutputType.FILE); // **
+		element.getScreenshotAs(OutputType.BASE64);
+		element.getScreenshotAs(OutputType.BYTES);
+
+		// Tên thẻ HTML
+		// Dùng by.id/class/css/name
+		// Đầu ra của step này là đầu vào của step kia
+		element.findElement(By.cssSelector("#save-info-button"));
+		String saveButtonTagName = element.getTagName();
+		driver.findElement(By.xpath("//" + saveButtonTagName + "[@name='email']"));
+
+		String addressName = "123 Ly Thuong Kiet";
+		String cityName = "Ho Chi Minh City";
+
+		System.out.println(addressName + cityName);
+		System.out.println(addressName.concat(cityName));
+		System.out.println(addressName + "-" + cityName);
+
+		// Lấy text của header/link/label/error/success message
+		element.getText(); // **
+
+		// Verify dữ liệu
+		// Kiểm tra element có hiển thị hay không (người dùng có thể nhìn thấy được và
+		// thao tác được)
+		element.isDisplayed(); // **
+
+		// Kiểm trâ element có thao tác được hay không (không bị disable/ko phải
+		// readonly field)
+		element.isEnabled(); // **
+
+		// Kiểm tra element đã được chọn hay chưa (radio/checkbox/dropdown)
+		element.isSelected(); // **
+
+		// Submit vào 1 form
+		element.submit();
+	}
+
+	//@Test
+	public void TC_01_isDisplay() {
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+
+		WebElement emailTextBox = driver.findElement(By.id("mail"));
+		WebElement under18Radio = driver.findElement(By.id("under_18"));
+		WebElement eduTextArea = driver.findElement(By.id("edu"));
+		WebElement user5 = driver.findElement(By.xpath("//*[contains(text(),'User5')]"));
+
+		String userEmail = emailTextBox.getAttribute("name");
+		String under18Value = under18Radio.getAttribute("value");
+		String userEdu = eduTextArea.getAttribute("name");
+
+		if (emailTextBox.isDisplayed()) {
+			emailTextBox.clear();
+			emailTextBox.sendKeys("Automation Testing");
+			System.out.println(userEmail + " is displayed");
+		} else {
+			System.out.println(userEmail + " is not displayed");
+		}
+
+		if (under18Radio.isDisplayed()) {
+			under18Radio.click();
+			System.out.println(under18Value + " is displayed");
+		} else {
+			System.out.println(under18Value + " is not displayed");
+		}
 		
-		// Xóa dữ liệu
-		element.clear();
+		if (eduTextArea.isDisplayed()) {
+			eduTextArea.clear();
+			eduTextArea.sendKeys("Automation Testing");
+			System.out.println(userEdu + " is displayed");
+		} else {
+			System.out.println(userEdu + " is not displayed");
+		}
+		
+		if (user5.isDisplayed()) {
+			System.out.println("User5 is displayed");
+		} else {
+			System.out.println("User5 is not displayed");
+		}
+	}
+	
+	//@Test
+	public void TC_02_isEnable() {
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+		
+		List<WebElement> element = new ArrayList<WebElement>();
+		element.add(driver.findElement(By.id("email")));
+		element.add(driver.findElement(By.id("under_18")));
+		element.add(driver.findElement(By.id("edu")));
+		element.add(driver.findElement(By.id("job1")));
+		element.add(driver.findElement(By.id("job2")));
+		element.add(driver.findElement(By.id("development")));
+		element.add(driver.findElement(By.id("slider-1")));
+		element.add(driver.findElement(By.id("disable_password")));
+		element.add(driver.findElement(By.id("radio-disabled")));
+		element.add(driver.findElement(By.id("bio")));
+		element.add(driver.findElement(By.id("job3")));
+		element.add(driver.findElement(By.id("check-disbaled")));
+		element.add(driver.findElement(By.id("slider-2")));
+		
+		for (WebElement webElement : element) {
+			String elementName = webElement.getAttribute("id");
+			if (webElement.isEnabled()) {
+				System.out.println(elementName + " is enable");
+			} else {
+				System.out.println(elementName + " is disable");
+			}
+		}
+	}
+	
+	//@Test
+	public void TC_03_isSelected() {
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+		
+		WebElement under18Radio = driver.findElement(By.id("under_18"));
+		WebElement javaCheckBox = driver.findElement(By.id("java"));
+		
+		under18Radio.click();
+		javaCheckBox.click();
+		
+		if (under18Radio.isSelected()) {
+			System.out.println("Age Under 18 is selected");
+		} else {
+			System.out.println("Age Under 18 is de-selected");
+		}
+		
+		if (javaCheckBox.isSelected()) {
+			System.out.println("Java is selected");
+		} else {
+			System.out.println("Java is de-selected");
+		}
+	}
+
+	@Test
+	public void TC_04_Register_function_at_Mailchimp() {
+		driver.get("https://login.mailchimp.com/signup/");
+		
+		WebElement emailTextBox = driver.findElement(By.id("email"));
+		WebElement userNameTextBox = driver.findElement(By.id("new_username"));
+		//WebElement passwordTextBox = driver.findElement(By.id("new_password"));
+		
+		emailTextBox.sendKeys("automationfc@gmail.com");
+		emailTextBox.sendKeys(Keys.TAB);
+		
+		Assert.assertEquals(userNameTextBox.getAttribute("value"), emailTextBox.getAttribute("value"));
 	}
 	
 	@AfterClass
