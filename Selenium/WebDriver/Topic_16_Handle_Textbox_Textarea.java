1895d5d7b35b993c159a62ab5a345ebdb789a272
minhtien1650 <minh.tien.1650@gmail.com>
2024-08-22 12:52:33 +0700

    Update Topic_16_Handle_Textbox_Textarea.java
    update

--- Selenium/WebDriver/Topic_16_Handle_Textbox_Textarea.java
+++ Selenium/WebDriver/Topic_16_Handle_Textbox_Textarea.java
@@ -30,31 +30,31 @@ public class Topic_16_Handle_Textbox_Textarea {
 	public void TC_01() {
 		driver.get("http://demo.guru99.com/");
 
-		driver.findElemnet(By.xpath("//input[@name='emailid']")).sendKeys(email);
-		driver.findElemnet(By.xpath("//input[@name='btnLogin']")).click();
+		driver.findElement(By.xpath("//input[@name='emailid']")).sendKeys(email);
+		driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
 
 		String userID = driver.findElement(By.xpath("//td[contains(.,'User ID :')]/following-sibling::td")).getText();
 		String password = driver.findElement(By.xpath("//td[contains(.,'Password :')]/following-sibling::td")).getText();
 		
 		driver.get("http://demo.guru99.com/v4");
 
-		driver.findElemnet(By.name("uid")).sendKeys(userID);
-		driver.findElemnet(By.name("password")).sendKeys(password);
+		driver.findElement(By.name("uid")).sendKeys(userID);
+		driver.findElement(By.name("password")).sendKeys(password);
 
 		Assert.assertTrue(driver.findElement(By.xpath("//marquee[@class='heading3']")).getText().contains("Welcome To Manager's Page of Guru99 Bank"));
 
-		driver.findElemnet(By.xpath("//a[contains(.,'New Customer')]")).click();
+		driver.findElement(By.xpath("//a[contains(.,'New Customer')]")).click();
 
-		driver.findElemnet(By.xpath("//input[@onkeyup='validatecustomername();']")).sendKeys("Selenium Online");
-		driver.findElemnet(By.xpath("//input[@value='m']")).click();
-		driver.findElemnet(By.xpath("//input[@onkeyup='validatedob();']")).sendKeys("10012000");
-		driver.findElemnet(By.xpath("//input[@onkeyup='validateAddress();']")).sendKeys("123 Address");
-		driver.findElemnet(By.xpath("//input[@onkeyup='validateCity();']")).sendKeys("Ho Chi Minh");
-		driver.findElemnet(By.xpath("//input[@onkeyup='validateState();']")).sendKeys("Thu Duc");
-		driver.findElemnet(By.xpath("//input[@onkeyup='validatePIN();']")).sendKeys("123456");
-		driver.findElemnet(By.xpath("//input[@onkeyup='validateTelephone();']")).sendKeys("0123456789");
-		driver.findElemnet(By.xpath("//input[@onkeyup='validateEmail();']")).sendKeys(email);
-		driver.findElemnet(By.xpath("//input[@onkeyup='validatepassword();']")).sendKeys(password);
+		driver.findElement(By.xpath("//input[@onkeyup='validatecustomername();']")).sendKeys("Selenium Online");
+		driver.findElement(By.xpath("//input[@value='m']")).click();
+		driver.findElement(By.xpath("//input[@onkeyup='validatedob();']")).sendKeys("10012000");
+		driver.findElement(By.xpath("//input[@onkeyup='validateAddress();']")).sendKeys("123 Address");
+		driver.findElement(By.xpath("//input[@onkeyup='validateCity();']")).sendKeys("Ho Chi Minh");
+		driver.findElement(By.xpath("//input[@onkeyup='validateState();']")).sendKeys("Thu Duc");
+		driver.findElement(By.xpath("//input[@onkeyup='validatePIN();']")).sendKeys("123456");
+		driver.findElement(By.xpath("//input[@onkeyup='validateTelephone();']")).sendKeys("0123456789");
+		driver.findElement(By.xpath("//input[@onkeyup='validateEmail();']")).sendKeys(email);
+		driver.findElement(By.xpath("//input[@onkeyup='validatepassword();']")).sendKeys(password);
 		driver.findElement(By.xpath("input[value='Submit']")).click();
 
 		System.out.println(driver.findElement(By.xpath("//td[contains(text(),'Customer ID')]//following-sibling::td")).getText());
